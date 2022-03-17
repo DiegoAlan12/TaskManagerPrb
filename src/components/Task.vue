d57f47baf1f1a322a5977b89c022fe2f4c6ae05b
DiegoAlan12 <g3diego.321.p5@gmail.com>
2022-03-16 18:21:05 -0600

    first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello World From Vue</h1>
+  <p>Hello {{ name }} from Task Componen </p>
+</template>
+
+<script>
+
+export default {
+    props: ['name'],
+}
+</script>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello world from vue</h1>
+  <input type="text" v-model="name">
+
+  <Task :name="name"/>
+
+</template>
+
+<script>
+
+import Task from "../components/Task.vue";
+
+export default {
+  name: 'Home',
+  components: {
+    Task
+  },
+  data(){
+    return{
+      name: ''
+    }
+  }
+}
+</script>
